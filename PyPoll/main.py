74d22472193cd643b9a96fcd66f8d2598e9aab6f
PrairieDogCity <42921169+PrairieDogCity@users.noreply.github.com>
2018-10-13 23:26:08 -0500

    Add files via upload (Final outputs here)

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,30 +1,61 @@
 # This script analyzes votes cast in an election, and calculates:
-#   The total number of votes cast
-#   A complete list of candidates who received votes
-#   The percentage of votes each candidate won
-#   The total number of votes each candidate won
-#   The winner of the election based on popular vote.
+#   -The total number of votes cast
+#   -A complete list of candidates who received votes
+#   -The percentage of votes each candidate won
+#   -The total number of votes each candidate won
+#   -The winner of the election based on popular vote.
 # It also prints the analysis to the terminal, and exports a text file
 # with the results. 
-
-  #Election Results (EXAMPLE)
-  #-------------------------
-  #Total Votes: 3521001
-  #-------------------------
-  #Khan: 63.000% (2218231)
-  #Correy: 20.000% (704200)
-  #Li: 14.000% (492940)
-  #O'Tooley: 3.000% (105630)
-  #-------------------------
-  #Winner: Khan
-  #-------------------------
 
 # import pandas to help manage and analyze the budget data
 import pandas as pd
 
 # Use pandas to read the data
-df_votes = pd.read_csv("../PyPoll/election_data.csv", encoding = "utf-8", sep=",")
+votes = "election_data.csv"
+df_votes = pd.read_csv(votes)
 
-# Use pandas for a cursory look at the data
-print(df_votes.head())
+# Calculate total number of votes cast
+total_votes = df_votes["Voter ID"].count()
+
+# Compute complete list of candidates who received votes
+got_votes = df_votes["Candidate"].unique()
+
+# Calculate the percentage of votes each candidate received
+grouped_vote = df_votes.groupby(["Candidate"]).count()
+perc_vote = (grouped_vote / total_votes)
+perc_vote_sized = perc_vote.drop(columns=["County"])*100
+
+# Calculate the total number of votes each candidate received
+votes_received = grouped_vote.drop(columns=["County"])
+
+# Calculate the winner of the election based on popular vote
+winner = votes_received.sort_values(["Voter ID", "Candidate"], ascending=False)
+
+# Rename "Voter ID" column to "Percent of Vote"
+perc_vote_sized_rename = perc_vote_sized.rename(columns={'Voter ID': 'Percent of Vote'})
+
+# Rename "Voter ID" column to "Percent of Vote"
+votes_received_rename = votes_received.rename(columns={'Voter ID': 'Total Votes'})
+
+# Join percent and total vote dataframes
+joined_summary = perc_vote_sized_rename.join(votes_received_rename)
+
+# Change percent of vote formatting
+joined_summary["Percent of Vote"]=joined_summary["Percent of Vote"].map("{:.3f}%".format)
+
+#Sort and explicitly define a sorted summary dataframe
+joined_summary_sorted = joined_summary.sort_values(by=['Total Votes'], ascending=False)
+
+#Print Analysis to Terminal
+print("Election Results")
+print("----------------------------------")
+print("Total Votes: " + str(total_votes))
+print("----------------------------------")
+print(joined_summary_sorted)
+print("----------------------------------")
+print("Winner: "+ winner.index[0])
+print("----------------------------------")
+
+# Export summary_table to .csv file
+joined_summary_sorted.to_csv("vote_output.csv", index=True, header=True)
 
